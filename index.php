c5fc5bb3c648a05438262c0ba9694fac61b079f9
Maciej Rudnicki -=GizY=- <maciejrudnickipl@gmail.com>
2018-08-04 12:12:09 +0200

    error fix small change:  - change of name class  - add output dir image  - check if img to protect is good  - change div size for rescale

--- index.php
+++ index.php
@@ -17,29 +17,42 @@
 <main>
 
     <div class="c">
-        <?php
-        include 'gizySecPic.php';
-        echo imageSecure('srcImg/kot2.jpg', 0, 0);
-//        echo imageSecure('imgSrc/kot2.jpg', 0, 0);
 
-        ?>
+        <div class="title">TESTOWY OBRAZEK 1 s</div>
+        <div class="gipImage">
+            <?php
+            include 'Gip.php';
+            $gip = new \gizyGip\Gip('srcImg/im1.jpg');   //image to protect
+            $gip->createProtectImg(); //no resize
+            echo $gip->showAlerts();
+            echo $gip->getHtmlImgProtect();
+            ?>
+        </div>
+
+        <div class="title">TESTOWY OBRAZEK 2 zmiana wielkości</div>
+        <div class="gipImage">
+            <?php
+            $gip = new \gizyGip\Gip('srcImg/im2.jpg');   //image to protect
+            $gip->createProtectImgResize(500, 200); //resize
+            echo $gip->getHtmlImgProtect();
+            ?>
+        </div>
+
+        <div class="title">TESTOWY OBRAZEK 3 przeskalowanie</div>
+        <div class="gipImage">
+            <?php
+            $gip = new \gizyGip\Gip('srcImg/image3.jpg');   //image to protect
+            $gip->createProtectImgResize(400, null); //resize by witdh
+            echo $gip->getHtmlImgProtect();
+            ?>
+        </div>
+
+
     </div>
 
 
 </main>
-
-
 <footer>
-
-
 </footer>
 </body>
 
-
-<script type="application/ecmascript">
-
-    var board = new HelpBoard('myBoard');
-    var info = new HelpInfo('Info');
-    board.add(info);
-
-</script>
